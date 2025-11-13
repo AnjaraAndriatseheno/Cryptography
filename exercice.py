38bcb5bed38dbdd39f6e3b58089bd68125a66e34
AnjaraAndriatseheno <nanjaraandria@gmail.com>
2025-11-13 09:09:41 +0100

    feature : exo finit

--- exercice.py
+++ exercice.py
@@ -1,20 +1,17 @@
 import string
 
-def cesar_cipher(text, key):
+def cesar_cipher(text, key, cipher):
 	if type(text) == str and type(key) == int:
-		return "".join([chr((ord(char) + key) % 1_114_112) for char in text])
+		return "".join([chr((ord(char) + (1 if cipher else -1) * key) % 1_114_112)\
+																for char in text])
 	else:
 		raise(TypeError)
-
-
-def cesar_uncipher(crypted_text, key):
-		return cesar_cipher(crypted_text, -key)
 
 
 def hack_cesar_cipher(crypted_text, alphabet):
 	if type(crypted_text) == str and type(alphabet) == str:
 		for possible_key in range(0, 1_114_112):
-			possible_uncryption = cesar_uncipher(crypted_text, possible_key)
+			possible_uncryption = cesar_cipher(crypted_text, possible_key, cipher=False)
 			if possible_uncryption[0] in alphabet:
 				print(possible_key)
 				print(possible_uncryption)
@@ -23,54 +20,34 @@ def hack_cesar_cipher(crypted_text, alphabet):
 		raise(TypeError)
 
 
-def vigenere_cipher(text, password,key):
+def vigenere_cipher(text, password, cipher):
+	list_of_crypted_chars = []
 	list_of_keys = [ord(char) for char in password]
-	crypted_text = []
-	key_length = len(password)
-	for index, char in enumerate(text):
-		current_key = list_of_keys[index % len(list_of_keys)]
-		current_key = list_of_keys[index % key_length]
-		crypted_text.append(cesar_cipher(char, current_key))
-		if type(text) == str and type(key) == int :
-		    return "".join(crypted_text)
+	
+	for index, current_char in enumerate(text):
 		
+		current_key = list_of_keys[index % len(list_of_keys)]
+		current_crypted_char = cesar_cipher(current_char, current_key, cipher)
 
-        
+		list_of_crypted_chars.append(current_crypted_char)
 
+	crypted_text = "".join(list_of_crypted_chars)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	return crypted_text
 
 
 if __name__ == "__main__":
 	message = "le chocolat est bon"
 
-	crypted_text = cesar_cipher(message, 12) # exo 1
+	crypted_text = cesar_cipher(message, 12, cipher=True) # exo 1
 	print(crypted_text)
 
-	initial_message = cesar_uncipher(crypted_text, 12) # exo 2
+	initial_message = cesar_cipher(crypted_text, 12, cipher=False) # exo 2
 	print(initial_message == message)
 
 	hack_cesar_cipher(crypted_text, alphabet=string.printable) # exo3
 
-message = "Aujourd'hui nous sommes mercredi "
-password = "journee"
-result = vigenere_cipher(message, password)
-print(result)
+	crypted_message = vigenere_cipher(text=message, password="Azerty12345!", cipher=True)
+	print(crypted_message)
+	initial_message = vigenere_cipher(text=crypted_message, password="Azerty12345!", cipher=False)
+	print(initial_message)
