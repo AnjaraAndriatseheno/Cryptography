f75274c02e9d01cf50a3fb8f5f5edf06cfb6bc86
AnjaraAndriatseheno <nanjaraandria@gmail.com>
2025-11-13 20:15:37 +0100

    convert pixel

--- steganographie.py
+++ steganographie.py
@@ -4,7 +4,7 @@ import cv2
 
 img = cv2.imread('fire_force.jpg')
 
-cv2.imshow('Image', img)
+cv2.imshow('Image originel', img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
@@ -13,25 +13,30 @@ def affichage_pixel (img, x, y):
     pixel_val = img[x, y]
     print(f"Valeur du pixel {x}, {y} : {pixel_val}")
 
-# def convert_pixel (pixel_val) : 
-#     if pixel_val % 2 == 0 : 
-#         pixel_val = pixel_val+ 1
-#         print(pixel_val)
-#     else : 
-#         print(pixel_val)
+def convertir_pixels_pairs(img):
+    img_paire = img.copy()
+    img_paire = img_paire - (img_paire % 2)
+    return img_paire
 
-# affichage_pixel(img, 60, 100)
+affichage_pixel(img, 60, 100)
 
+img_pairs = convertir_pixels_pairs(img)
+cv2.imshow('Image convertie (valeurs paires)', img_pairs)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-
-
-def text_to_binary(text) :
-    binary_result = "".join(format(ord(char), '08b') for char in text)
-    return binary_result
+affichage_pixel(img_pairs, 60, 100)
 
 
-if __name__ == "__main__":
-    message = input("Entrer un message à convertir en binaire :")
 
-print(f"Texte original : {message}")
-print(f"Binaire : {text_to_binary(message)}")
+
+# def text_to_binary(text) :
+#     binary_result = "".join(format(ord(char), '08b') for char in text)
+#     return binary_result
+
+
+# if __name__ == "__main__":
+#     message = input("Entrer un message à convertir en binaire :")
+
+# print(f"Texte original : {message}")
+# print(f"Binaire : {text_to_binary(message)}")
