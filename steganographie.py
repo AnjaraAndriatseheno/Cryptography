a8e8eae69a23ae3f030f947addfe5996f08d92cd
AnjaraAndriatseheno <nanjaraandria@gmail.com>
2025-11-13 14:22:37 +0100

    feature : pixel

--- steganographie.py
+++ steganographie.py
@@ -1,7 +1,13 @@
 import cv2
 
-image = cv2.imread('fire_force.jpg')
+img = cv2.imread('fire_force.jpg')
 
-cv2.imshow('Image', image)
+cv2.imshow('Image', img)
 
 cv2.waitKey(0)
+
+def affichage_pixel (img, x, y):
+    pixel_val = img[x, y]
+    print(f"Valeur du pixel {x}, {y} : {pixel_val}")
+
+affichage_pixel(img, 60, 100)
