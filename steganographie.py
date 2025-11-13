3d60a63ac4e6435b17229eb302f61fc994850b84
AnjaraAndriatseheno <nanjaraandria@gmail.com>
2025-11-13 16:58:53 +0100

    essai exo3

--- steganographie.py
+++ steganographie.py
@@ -2,19 +2,25 @@ import cv2
 
 # Affichage d'image 
 
-# img = cv2.imread('fire_force.jpg')
+img = cv2.imread('fire_force.jpg')
 
-# cv2.imshow('Image', img)
+cv2.imshow('Image', img)
 
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# def affichage_pixel (img, x, y):
-#     pixel_val = img[x, y]
-#     print(f"Valeur du pixel {x}, {y} : {pixel_val}")
+def affichage_pixel (img, x, y):
+    pixel_val = img[x, y]
+    print(f"Valeur du pixel {x}, {y} : {pixel_val}")
+
+# def convert_pixel (pixel_val) : 
+#     if pixel_val % 2 == 0 : 
+#         pixel_val = pixel_val+ 1
+#         print(pixel_val)
+#     else : 
+#         print(pixel_val)
 
 # affichage_pixel(img, 60, 100)
-
 
 
 
